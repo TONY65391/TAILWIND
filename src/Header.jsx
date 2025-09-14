833754f2e28dc3d8f55f5558bd0e5554c52d22a7
TONY65391 <tonyanthony65391@gmail.com>
2025-09-14 21:27:34 +0100

    images

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+
+const handleNavClick = () => {
+    const nav = document.querySelector('header nav');
+    if (nav.classList.contains('open')) nav.classList.remove('open');
+    else{nav.classList.add('open');}
+    console.log(nav.classList);
+}
+
 function Header(){
+
     return(
-        <header className="flex items-center justify-between px-10 py-3">
+        <header className="flex items-center justify-between px-5 md:px-10 py-5">
             <div>
-                <h1 className="text-[25px] text-orange-500">Levitating Monk</h1>
+                <h1 className="text-[25px] text-orange-400">Levitating Monk</h1>
             </div>
 
             <div>
                 <ul className="items-center space-x-10 hidden md:flex">
-                    <li className=":after:"><a className="text-orange-500 cursor-normal" href="#">Home</a></li>
+                    <li className=":after:"><a className="text-orange-400 cursor-normal" href="#">Home</a></li>
                     <li><a className="hover:text-orange-500" href="#">About</a></li>
                     <li><a className="hover:text-orange-500" href="#">Techniques</a></li>
                     <li><a className="hover:text-orange-500" href="#">Reach</a></li>
@@ -15,13 +25,12 @@ function Header(){
                 </ul>
             </div>
 
-            <nav className="md:hidden h-10 w-10 cursor-pointer space-y-1 flex flex-col items-center justify-center">
-                <div className="h-1 w-[80%] rounded-md bg-orange-500"></div>
-                <div className="h-1 w-[80%] rounded-md bg-orange-500"></div>
-                <div className="h-1 w-[80%] rounded-md bg-orange-500"></div>
+            <nav onClick={() => {handleNavClick()}} className="open:border-2 open:border-blue-500 md:hidden group h-10 w-10 cursor-pointer space-y-1 flex flex-col items-center justify-center">
+                <div className="h-1 w-[60%] rounded-md bg-orange-500 group-hover:bg-white"></div>
+                <div className="h-1 w-[60%] rounded-md bg-orange-500 group-hover:bg-white"></div>
+                <div className="h-1 w-[60%] rounded-md bg-orange-500 group-hover:bg-white"></div>
             </nav>
         </header>
     )
 }
-
 export default Header;
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,24 +1,28 @@
 
 function Landing(){
+    const shoot = (a, b) => {
+        console.log(a);
+        alert(b.type)
+    }
     return(
         <>
             <div className="h-screen block md:flex item-center justify-around md:mt-[0px] mt-[100px]">
-                <div className="w-full px-10 flex flex-col items-center justify-center">
+                <div className="w-full px-5 md:px-10 flex flex-col items-center justify-center">
 
                     <p className="w-full text-[25px] tracking-widest">TonDev</p>
 
-                    <h1 className="text-[35px] capitalize small-caps mb-3"><span className="text-orange-500">levitating</span> and <span className="text-orange-500">meditating monk</span> in a <span className="text-orange-500">serene landscape</span></h1>
+                    <h1 className="text-[35px] capitalize small-caps mb-3"><span className="text-orange-400">levitating</span> and <span className="text-orange-400">meditating monk</span> in a <span className="text-orange-400">serene landscape</span></h1>
 
-                    <p className="leading-[1.5]">In a quiet mountain temple lived Tenzu, a monk draped in flowing orange robes, the color of dawn and renewal. He walked barefoot along stone paths, carrying no possessions but his staff and calm smile. The villagers whispered that his orange garments did not merely cover him—they glowed faintly at sunset, as if the fabric had absorbed the sky’s fire. Tenzu believed silence was a greater teacher than words, and those who sat with him often left lighter, as though a hidden burden had been lifted.</p>
+                    <p className="leading-[1.5]">In a quiet mountain temple lived Tenzu, a monk draped in flowing orange robes, the color of dawn and renewal. He walked barefoot along stone paths, carrying no possessions but his staff and calm smile. The villagers whispered that his orange garments did not merely cover him—they glowed faintly at sunset, as if the fabric had absorbed the sky’s fire. Tenzu believed silence was a greater teacher than words, and those who sat with him often left lighter, as though a hidden burden had been lifted. Tony Updated</p>
 
                     <div className="w-full h-[50px] flex items-center md:space-x-5 space-x-2 mt-7">
-                        <button className="border-2 border-orange-500 text-black tracking-widest font-semibold md:px-7 px-5 md:py-3 py-2 rounded-md bg-orange-500 hover:bg-transparent hover:text-white cursor-pointer small-caps">Subscribe</button>
-                        <button className="md:px-7 px-5 md:py-3 py-2 border-2 border-orange-500 rounded-md font-semibold small-caps hover:bg-orange-500 hover:text-black cursor-pointer">Learn More</button>
+                        <button className="border-2 border-orange-500 text-black tracking-widest font-semibold md:px-7 px-5 md:py-3 py-2 rounded-md bg-orange-400 hover:bg-transparent hover:text-white cursor-pointer small-caps">Subscribe</button>
+                        <button className="md:px-7 px-5 md:py-3 py-2 border-2 border-orange-500 rounded-md font-semibold small-caps hover:bg-orange-400 hover:text-black cursor-pointer">Learn More</button>
                     </div>
                 </div>
 
                 <div className="w-full gaze">
-                    <img className="w-full h-auto round-md myJumpAnimation" src="/src/assets/Floating_Monk_with_Dragon_Staff-removebg-preview.png" alt="" />
+                    <img className="w-full h-auto round-md myJumpAnimation" src="src/assets/Floating_Monk_with_Dragon_Staff-removebg-preview.png" alt="Tenzu, the floating monk" />
                 </div>
             </div>
         </>
